551a8161778d511baf1879f44a589d8f3d80d9d9
tanvidhope <43072835+tanvidhope@users.noreply.github.com>
2018-09-13 19:24:51 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 import random
 import csv
+import re
 dicti={} #dictionary
 with open('data.csv', mode='w') as csv_file:
     fieldnames = ['character', 'frequency']
@@ -9,24 +10,21 @@ with open('data.csv', mode='w') as csv_file:
     rob=open('fileWork.txt','r') #read object
     listme=rob.readlines()
     for x in listme:
-        words=x.split()
+        words = re.findall(r"[\w]+|[^\s\w]",x)
         for y in words:
             ch=random.choice(y)
             if(ch in dicti):
                 dicti[ch]=dicti.get(ch)+y.count(ch)
             else:
                 dicti[ch]=y.count(ch)
-            print(ch,y.count(ch))
-    print (dicti)
+    #print (dicti)
     st='a'
-    flag=0
     while st<='z':
+        flag=0
         for c in dicti:
             if(c==st):
-                row=[st,dicti[c]]
                 writer.writerow({'character':st,'frequency':dicti[c]})
                 flag=1
-
         if(flag==0):
             writer.writerow({'character':st,'frequency':'0'})
         st=chr(ord(st) + 1)
